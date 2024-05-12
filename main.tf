0ff1aabc2745938ac248d37bd6cbff934f8e8a23
Rohmat <rohmat.trie@gmail.com>
2024-05-12 18:59:11 +0700

    update

---
+++ main.tf
@@ -0,0 +1,12 @@
+# module "s3_bucket" {
+#   source  = "terraform-aws-modules/s3-bucket/aws"
+#   version = "~> 4.1.2"
+
+#   bucket = "mugiwara-unique-bucket-${random_pet.bucket_id.id}"
+#   acl    = "private"
+# }
+
+# resource "random_pet" "bucket_id" {
+#   length    = 3
+#   separator = "-"
+# }
